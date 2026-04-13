629f145b1440f5536ab5a832b02fa57cd983f3ed
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-04-13 21:09:14 +0100

    Initial Commit

---
+++ forgedb/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  forgedb
+//
+//  Created by Chidume Nnamdi on 13/04/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
